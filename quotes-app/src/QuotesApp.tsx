3b91c0f3fdc3c613046b56ba08408ea628db603a
Jesús Mata <andresmata.c21@gmail.com>
2023-01-26 22:13:17 -0600

    Inicio de la prueba - quotes

---
+++ quotes-app/src/QuotesApp.tsx
@@ -0,0 +1,14 @@
+interface Props {}
+
+export const QuotesApp = ({}: Props) => {
+  return (
+    <>
+      <h1
+        className="text-3xl font-bold underline"
+        style={{ backgroundColor: 'aquamarine' }}
+      >
+        Hello world!
+      </h1>
+    </>
+  )
+}
--- quotes-app/src/main.tsx
+++ quotes-app/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { QuotesApp } from './QuotesApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <QuotesApp />
+  </React.StrictMode>
 )
